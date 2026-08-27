8db35be312c7a647b3c0122b11b3ef1da4b5f1b9
Davi <smartmedsprojects@gmail.com>
2026-08-27 16:53:32 -0300

    Davi

--- Projeto/models/item_saida.py
+++ Projeto/models/item_saida.py
@@ -1,80 +1,44 @@
 from core.crud_base import CrudBase
 from core.database import Database
 from core.validator import Validator
-
-
 class ItemSaida(CrudBase):
-
     table = "item_saida"
-
     fields = [
         "quantidade",
         "valor",
         "pedido_saida_id",
-<<<<<<< HEAD
-        "movimentacao_id"
-=======
         "movimentacao_id",
->>>>>>> 3e8710a53226466da3f72d48d915e91247e78511
     ]
-
-    def __init__(
-        self,
-        quantidade,
-        valor,
-        pedido_saida_id,
-<<<<<<< HEAD
-        movimentacao_id
-=======
-        produto_id
->>>>>>> 3e8710a53226466da3f72d48d915e91247e78511
-    ):
+    def __init__(self, quantidade, valor, pedido_saida_id, produto_id):
         self.quantidade = int(quantidade)
         self.valor = float(valor)
         self.pedido_saida_id = int(pedido_saida_id)
-<<<<<<< HEAD
-        self.movimentacao_id = int(movimentacao_id)
-
-=======
-
-        # Mantemos produto_id no objeto para o endpoint usar,
-        # mas ele NÃO será inserido na tabela item_saida.
         self.produto_id = int(produto_id)
-
-        # Será preenchido antes do insert()
         self.movimentacao_id = None
 
->>>>>>> 3e8710a53226466da3f72d48d915e91247e78511
     def validate(self):
         erros = [
             Validator.required(self.quantidade, "Quantidade"),
             Validator.required(self.valor, "Valor"),
             Validator.required(self.pedido_saida_id, "Pedido"),
-            Validator.required(self.movimentacao_id, "Movimentação"),
+            Validator.required(self.produto_id, "Produto"),
         ]
-
         return [erro for erro in erros if erro]
-
+        
     @classmethod
     def find_by_pedido(cls, pedido_id):
         conexao = Database.connect()
         cursor = conexao.cursor(dictionary=True)
-
         try:
             sql = """
                 SELECT
                     i.id,
                     i.quantidade,
                     i.valor,
-<<<<<<< HEAD
-                    p.nome AS produto,
-                    p.id AS produto_id
-=======
                     i.pedido_saida_id,
                     i.movimentacao_id,
                     m.produto_id,
                     p.nome AS produto
->>>>>>> 3e8710a53226466da3f72d48d915e91247e78511
                 FROM item_saida i
                 INNER JOIN movimentacao m
                     ON i.movimentacao_id = m.id
@@ -83,10 +47,8 @@ class ItemSaida(CrudBase):
                 WHERE i.pedido_saida_id = %s
                 ORDER BY i.id DESC
             """
-
             cursor.execute(sql, (pedido_id,))
             return cursor.fetchall()
-
         finally:
             cursor.close()
             conexao.close()
